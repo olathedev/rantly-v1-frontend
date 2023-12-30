037f9f84d5c88ac8d46447c672d57120a2cfbd59
Micheal Ojedeji <97312093+DevOlaMicheal@users.noreply.github.com>
2023-12-30 12:31:07 +0100

    bug fix

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import DashboardNav from '../components/dashboardcomps/dashboardNav'
+import DashboardNav from '../components/dashboardcomps/DashboardNav'
 import { Route, Routes } from 'react-router-dom'
 import User from '../pages/User'
 import Rants from '../pages/Rants'
