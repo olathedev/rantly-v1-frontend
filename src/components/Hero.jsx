7b727f7432e27b2c4327a7fed58f6724b3f8c711
Micheal Ojedeji <97312093+DevOlaMicheal@users.noreply.github.com>
2023-12-30 12:42:29 +0100

    bug fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,12 @@ function Hero() {
   return (
     <div className='container text-center px-3 mx-auto flex flex-col-reverse md:px-10 mt-4 md:flex-row md:justify-between md:text-start '>
         <div className="pt-10 md:w-1/2 md:pt-20 flex-col gap-4">
-            <div className="text-4xl md:text-5xl font-ws font-semibold text-gray-900">A Cool way <br></br>for your friends to rant on you <span className="font-bold text-primary">Anonymously.</span></div>
-            <div className='font-mont md:w-2/3 mt-6 text-xl text-gray-800 font-medium'> Rantly is Sleek and <span className="font-bold">Super Easy to use</span>. Create an account, share link with friends and start getting anonymous rants on the go </div>
+            <div className="text-4xl md:text-5xl font-ws font-semibold text-gray-900">A Cool way <br></br>to get <span className="font-bold text-primary">Anonymous Messages.</span></div>
+            <div className='font-mont md:w-2/3 mt-6 text-lg text-gray-800 font-medium'> Rantly is Sleek and <span className="font-bold">Super Easy to use</span>. Create an account, share link with friends and start getting anonymous rants on the go </div>
             <div className='flex justify-center gap-4 items-center mt-6 md:justify-start'>
              
-             <Link to='dasboard/user'> <button className="bg-primary text-white text-xl font-pop py-3 px-6 rounded-lg">Start Ranting</button> </Link>
-                <button className="border border-primary text-xl font-pop py-3 px-6 rounded-lg">Learn more</button>
+             <Link to='dashboard/user'> <button className="bg-primary text-white text-xl font-pop py-2 px-2 md:py-3 md:px-6 rounded-lg">Start Ranting</button> </Link>
+                <button className="border border-primary text-xl font-pop py-2 px-2 md:py-3 md:px-6 rounded-lg">Learn more</button>
 
 
             </div>
