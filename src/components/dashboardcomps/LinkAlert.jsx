4e91d58a9125e12ef6bb4dc226d9959fd39b762e
DevOlaMicheal <olapademicheal3@gmail.com>
2024-01-24 11:48:18 -0800

    Implemeted Copy to cliboard

--- src/components/dashboardcomps/LinkAlert.jsx
+++ src/components/dashboardcomps/LinkAlert.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import { MdCopyAll } from 'react-icons/md'
 
 function LinkAlert() {
+
+  const copyToClipBoard = (text) => {
+    navigator.clipboard.writeText(text)
+    alert(text + " " + "Copied")
+  }
+
   return (
     <div>
     <div className="alert bg-primary bg-opacity-25 border-2 border-primary rounded-lg py-3 px-6 font-pop text-center">Here's your rant link
-     <div className="font-medium font-ws bg-[#040406] p-2 rounded-md text-white mt-2 flex items-center justify-between">rantly.vercel.app/oladev01 <MdCopyAll /> </div>
+     <div className="text-center font-medium font-ws bg-[#040406] p-3 rounded-md text-white mt-2 flex items-center justify-between cursor-pointer">rantly.vercel.app/oladev01 <div onClick={() => copyToClipBoard("rantly.vercel.app/oladev01")}> <MdCopyAll /> </div> </div>
 
 <div className="flex flex-col gap-4 mt-6 justify-center items-center">
 
