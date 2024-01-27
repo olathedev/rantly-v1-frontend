0aeb48579d4e462a53f37a95dacd43c1fd9058fd
DevOlaMicheal <olapademicheal3@gmail.com>
2024-01-27 10:23:13 +0100

    finishe mobile nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 
@@ -6,14 +6,14 @@ import { Link } from "react-router-dom";
 function Nav() {
 
   const [scrollClasses, setScrollClasses] = useState(false)
-  const [navOpen, setNavOen] = useState(false)
+  const [navOpen, setNavOpen] = useState(false)
 
   const changeNavColor = () => {
-    if(window.scrollY >= 80) {
+    if (window.scrollY >= 80) {
       setScrollClasses(true)
-    }else{
+    } else {
       setScrollClasses(false)
-  }
+    }
   }
   window.addEventListener('scroll', changeNavColor)
 
@@ -37,13 +37,13 @@ function Nav() {
           <Link to={"dashboard/user"}>
             {" "}
             <button className="bg-primary text-white text-lg font-ws py-3 px-6 rounded-md cursor-pointer">
-              Signup 
+              Signup
             </button>
           </Link>
           <Link to="dashboard/user">
-          
+
             <button className="border border-primary text-primary text-lg font-pop font-semibold py-3 px-6 rounded-md cursor-pointer">
-              Signin 
+              Signin
             </button>
           </Link>
         </div>
@@ -51,14 +51,13 @@ function Nav() {
 
       {/* mobile */}
 
-      <div className="container mx-auto px-6 py-2 flex justify-between items-center md:hidden">
+    <div className="md:hidden container mx-auto px-6 py-2">
+      <div className=" flex justify-between items-center">
         <div className="navbar-brand text-2xl font-ws font-semibold">
           Rantly
         </div>
 
-        <button className="font-semibold" onClick={() => setNavOen(!navOpen)}>
-
-          <span></span>
+        <button className="font-bold cursor-pointer" onClick={() => setNavOpen(!navOpen)}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -73,13 +72,44 @@ function Nav() {
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             />
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-8 h-8 ${navOpen ? 'block' : 'hidden'}`}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>
+        
+     
+        
 
 
           </svg>
+
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-8 h-8 ${navOpen ? 'block' : 'hidden'}`}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+</svg>
+
         </button>
+      </div>
+
+      <div className={`${navOpen ? 'block' : 'hidden'} pt-6 px-8 text-center`} onClick={() => setNavOpen(!navOpen)}>
+          <ul className="flex flex-col gap-3 text-lg font-pop font-semibold">
+            <li>About</li>
+            <li>Faq</li>
+            <li>Developer</li>
+
+            <div className="button flex flex-col pt-6 gap-2 w-full">
+
+            <Link to="dashboard/user">
+
+<button className="border border-primary text-primary text-lg font-pop font-semibold py-3 px-6 rounded-md cursor-pointer w-full">
+  Signin
+</button>
+</Link>
+          <Link to={"dashboard/user"}>
+            {" "}
+            <button className="bg-primary text-white text-lg font-ws py-3 px-6 rounded-md cursor-pointer w-full">
+              Signup
+            </button>
+          </Link>
+        
+        </div>
+          </ul>
+      </div>
       </div>
     </div>
   );
