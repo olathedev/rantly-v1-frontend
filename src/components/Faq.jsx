cd6732dc145903778a71604702f16644ec188dba
DevOlaMicheal <olapademicheal3@gmail.com>
2024-01-27 02:39:50 +0100

    uiFix

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -25,7 +25,7 @@ function Faq() {
                             <div className="accordion-head flex justify-between items-center px-6">
 
                             <h3 className="text-lg font-mont md:text-xl font-semibold">{faq.title}</h3>
-                            <div> 
+                            <div className='text-primary font-semibold'> 
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>
