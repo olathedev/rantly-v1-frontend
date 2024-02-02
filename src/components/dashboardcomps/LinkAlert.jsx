b28be8145f1d2b80a13d41f4e5075f79aee8d0bd
DevOlaMicheal <olapademicheal3@gmail.com>
2024-02-02 12:35:08 +0100

    Bug fix

--- src/components/dashboardcomps/LinkAlert.jsx
+++ src/components/dashboardcomps/LinkAlert.jsx
@@ -25,15 +25,14 @@ function LinkAlert({ user }) {
   }
   return (
     <div>
-      <div className="alert bg-black rounded-lg py-6 px-4 md:px-8 font-rale w-full">
+      <div className="container alert bg-black rounded-lg py-6 px-2 md:px-8 font-rale w-full">
 
-        <div className='flex items-center gap-2 justify-between bg-white rounded'>
-          <div className="text-center font-rale font-semibold p-2 rounded cursor-pointer text-black bg-opacity-40">
+        <div className='flex flex-wrap items-center gap-2  justify-center rounded'>
+          <div className="text-center font-rale font-semibold p-2 rounded cursor-pointer text-black bg-opacity-90 bg-white">
             {url}
-          </div>
+            </div>
 
-
-          <button onClick={() => copyToClipBoard(url)} className='bg-primary text-white p-2 rounded flex place-self-end' disabled={copied}>
+            <button onClick={() => copyToClipBoard(url)} className='bg-primary text-white p-2 rounded flex place-self-end' disabled={copied}>
             {!copied ? (
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 font-bols">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
@@ -49,6 +48,8 @@ function LinkAlert({ user }) {
             }
 
           </button>
+         
+         
         </div>
 
 
